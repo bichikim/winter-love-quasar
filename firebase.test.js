a360a28850e7cd2c6b7e21bd11c2ce361d4bc780
Bichi Kim <bichi@live.co.kr>
2019-11-26 06:06:34 +0900

    update

--- firebase.test.js
+++ firebase.test.js
@@ -1,14 +1,26 @@
 const firebase = require('@firebase/testing')
 const fs = require('fs')
+const {join} = require('path')
+const firebaseConfig = require('./firebase.json')
+const {database = {}} = firebaseConfig
 //
 // // start firebase mock [@firebase/testing]
 firebase.initializeTestApp({
   projectId: 'winter-love',
   auth: {uid: 'test', email: 'test@test.com'},
 })
+
+if(database.rules) {
+  const rules = fs.readFileSync(join(__dirname, database.rules), 'utf8')
 //
-firebase.loadDatabaseRules({
-  databaseName: 'winter-love',
-  rules: fs.readFileSync('./firestore.rules'),
-})
+  firebase.loadDatabaseRules({
+    databaseName: 'winter-love',
+    rules,
+  }).then(() => {
+    process.send('Database: loaded')
+  }).catch((e) => {
+    process.send(e)
+  })
+}
+
 //
