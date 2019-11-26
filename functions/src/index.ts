e993a5f05637067086a5a9600fcb87e734a0de32
Bichi Kim <bichi@live.co.kr>
2019-11-27 03:10:19 +0900

    add accessToken

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,11 @@
 import * as functions from 'firebase-functions'
 import * as firebase from 'firebase'
+import * as admin from 'firebase-admin'
+
+admin.initializeApp({
+  credential: admin.credential.applicationDefault(),
+  databaseURL: 'https://winter-love.firebaseio.com',
+})
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -13,17 +19,23 @@ export const getAccessToken = functions.https.onRequest((request, response) => {
   const user = firebase.auth().currentUser
   if(!user) {
     response.sendStatus(401).send('unauthenticated')
+    return
   }
 
   if(typeof request.body !== 'object') {
     response.send('wrong request')
+    return
   }
 
   const {token} = request.body
 
- if(!token) {
+  if(!token) {
     response.send('need token')
+    return
   }
 
+  response.send({
+    token: admin.auth().setCustomUserClaims(user.uid, {test: true}),
+  })
 
 })
