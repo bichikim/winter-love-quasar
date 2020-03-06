4da95d7f57fce36c8139f91275ebb2f6814e65ca
bichi <bichi@beecanvas.com>
2020-03-06 18:37:55 +0900

    update

--- test/build/env-reader.spec.ts
+++ test/build/env-reader.spec.ts
@@ -1,6 +1,32 @@
+import envReader from '../../build/env-reader'
 
 describe('env-reader', function test() {
   it('should read ', function test() {
-    expect(0).to.equal(0)
+    const env = {
+      'APP_FOO': 'Foo',
+      'APP_BAR': 'BAR',
+      'APP_JOHN': 'JOHN',
+      'NOT_THING': 'NOT_THING',
+    }
+    expect(envReader(env)).to.deep.equal({
+      'APP_FOO': '"Foo"',
+      'APP_BAR': '"BAR"',
+      'APP_JOHN': '"JOHN"',
+      },
+    )
+  })
+  it('should read', function test() {
+    const env = {
+      'VUE_FOO': 'Foo',
+      'VUE_BAR': 'BAR',
+      'VUE_JOHN': 'JOHN',
+      'NOT_THING': 'NOT_THING',
+    }
+    expect(envReader(env, 'VUE')).to.deep.equal({
+        'VUE_FOO': '"Foo"',
+        'VUE_BAR': '"BAR"',
+        'VUE_JOHN': '"JOHN"',
+      },
+    )
   })
 })
--- test/mocha.js
+++ test/mocha.js
@@ -1,6 +1,8 @@
 process.env.TS_NODE_TRANSPILE_ONLY = 'true'
 process.env.TS_NODE_PROJECT = 'tsconfig.node.json'
+process.env.NODE_ENV = 'test'
 // const tsNode = require('ts-node')
 // tsNode.register()
-require('@babel/register')
+const register = require('@babel/register').default
+register({extensions: ['.ts', '.tsx', '.js', '.jsx']})
 global.expect = require('chai').expect
